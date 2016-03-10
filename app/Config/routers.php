3ae1eff7b91c3289a1bf9cc69de35973f96bd27b
ale-php <alexandre@progs.net.br>
2016-03-10 18:24:26 -0300

    router melhorado usa-se ? para parametro

--- app/Config/routers.php
+++ app/Config/routers.php
@@ -8,4 +8,32 @@ $router->get('/', function(){
 
 });
 
+$router->get('/hello/?/?', function($name,$nome2) {
+    echo 'Hello ' . htmlentities($name.$nome2);
+});
+
+$router->mount('/site', function() use ($router) {
+
+    $router->get('/artigos/?/?', function($nome ,$sobrenome){
+
+
+        return (new HomeController())->artigos($nome,$sobrenome);
+
+    });
+
+    $router->get('/ebooks', function(){
+
+        return (new HomeController())->ebooks();
+
+    });
+
+    $router->get('/contato', function(){
+
+        return (new HomeController())->contatos();
+
+    });
+
+
+})
+
 ?>
--- app/Lib/Router.php
+++ app/Lib/Router.php
@@ -13,10 +13,50 @@ use Bramus\Router\Router as BaseRouter;
 
 class Router extends BaseRouter{
 
+    private $params;
 
     public function get($pattern, $fn) {
 
-        $this->match('GET', $pattern, $fn);
+
+        if($pattern == '/') {
+            $this->match('GET', $pattern, $fn);
+        }else{
+
+
+          $pattern = str_replace('?','(\w+)?',$pattern);
+
+                $this->match('GET',$pattern,$fn);
+            }
+
+
+
+
+
+    }
+
+    private function clean_string($string) {
+
+        if($string){
+            $string = str_replace(array('á','à','â','ã'),"a",$string);
+            $string = str_replace(array('Á','À','Â','Ã'),"A",$string);
+            $string = str_replace(array('é','è','ê'),"e",$string);
+            $string = str_replace(array('É','È','Ê'),"E",$string);
+            $string = str_replace(array('í','ì'),"i",$string);
+            $string = str_replace(array('Í','Ì'),"I",$string);
+            $string = str_replace(array('ó','ò','ô','õ','º'),"o",$string);
+            $string = str_replace(array('Ó','Ò','Ô','Õ'),"O",$string);
+            $string = str_replace(array('ú','ù','û'),"u",$string);
+            $string = str_replace(array('Ú','Ù','Û'),"U",$string);
+            $string = str_replace("ç","c",$string);
+            $string = str_replace("Ç","C",$string);
+            $string = str_replace(array('[',']','[','}','{',')'
+            ,'(',',',':',',',';','!','*','%','~','^','-',"'",'`','&',
+                '#',']','"',','),"",$string);
+             $string = str_replace(" ","",$string);
+            //  $string = str_replace(" ","_",$string);
+
+            return $string;
+        }
     }
 
 }
